f5b1ce047e00d8f0af5a551d1e0aea58ee48aca4
vishalicious213 <vishalicious213@gmail.com>
2020-03-17 20:06:31 -0400

    PostsPage: import useState. import data

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,9 +1,10 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from "../../dummy-data.js";
 
 const PostsPage = () => {
   // set up state for your data
