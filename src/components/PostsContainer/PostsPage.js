5f7c961c3695ad5ba4e8e2a9d7950c82cd817502
vishalicious213 <vishalicious213@gmail.com>
2020-03-17 20:45:23 -0400

    PostsPage: mapping through data

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -8,10 +8,15 @@ import dummyData from "../../dummy-data.js";
 
 const PostsPage = () => {
   // set up state for your data
-  const [postsData, setPostsData] = useState({dummyData});
+  const [postsData, setPostsData] = useState(dummyData);
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {/* map through data here to return a Post and pass data as props to Post */
+        postsData.map( (post) => {
+          console.log(post)
+          // <Post data={postsData} />
+        })
+        }
     </div>
   );
 };
